8f22eabbb8bc2d8a13a2503000acff4d723cdd4e
ramesh banakar <ramesh.s.b@campusuvce.in>
2026-04-11 22:00:25 +0530

    for all classes getters and setters are added and winning statergy implemented

--- src/Statergies/ColumnWise.java
+++ src/Statergies/ColumnWise.java
@@ -2,9 +2,23 @@ package Statergies;
 
 import Models.Move;
 
+import java.util.HashMap;
+
 public class ColumnWise implements WinningStatergies{
+    private int size;
+    private HashMap<String,Integer> colCount[];
+    ColumnWise(int size){
+        colCount = new HashMap[size];
+    }
     @Override
     public boolean checkWinner(Move move) {
-        return false;
+        int col=move.getCell().getCol();
+        String currSym=move.getPlayer().getSymbol().getSymbol();
+        HashMap<String,Integer> currCol=colCount[col];
+        if(!currCol.containsKey(currSym)){
+            currCol.put(currSym,1);
+        }else currCol.put(currSym,currCol.get(currSym)+1);
+
+        return size==currCol.get(currSym);
     }
 }
